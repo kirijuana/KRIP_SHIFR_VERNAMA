941f408de42512640e05b0dbd872e2ed6af55152
JussyFrussy <sazkirand@mail.ru>
2019-05-16 19:02:08 +0700

    Добавьте файлы проекта.

---
+++ KRIP_SHIFR_VERNAMA/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace KRIP_SHIFR_VERNAMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShifrText = new System.Windows.Forms.TextBox();
+            this.Text = new System.Windows.Forms.TextBox();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.button_unShifr = new System.Windows.Forms.Button();
+            this.button_Shifr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ShifrText
+            // 
+            this.ShifrText.Location = new System.Drawing.Point(100, 305);
+            this.ShifrText.Name = "ShifrText";
+            this.ShifrText.Size = new System.Drawing.Size(549, 20);
+            this.ShifrText.TabIndex = 9;
+            // 
+            // Text
+            // 
+            this.Text.Location = new System.Drawing.Point(100, 125);
+            this.Text.Name = "Text";
+            this.Text.Size = new System.Drawing.Size(549, 20);
+            this.Text.TabIndex = 8;
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(12, 206);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(282, 20);
+            this.keyBox.TabIndex = 7;
+            // 
+            // button_unShifr
+            // 
+            this.button_unShifr.Location = new System.Drawing.Point(275, 232);
+            this.button_unShifr.Name = "button_unShifr";
+            this.button_unShifr.Size = new System.Drawing.Size(217, 23);
+            this.button_unShifr.TabIndex = 6;
+            this.button_unShifr.Text = "Расшифровать";
+            this.button_unShifr.UseVisualStyleBackColor = true;
+            this.button_unShifr.Click += new System.EventHandler(this.button_unShifr_Click);
+            // 
+            // button_Shifr
+            // 
+            this.button_Shifr.Location = new System.Drawing.Point(275, 177);
+            this.button_Shifr.Name = "button_Shifr";
+            this.button_Shifr.Size = new System.Drawing.Size(217, 23);
+            this.button_Shifr.TabIndex = 5;
+            this.button_Shifr.Text = "Зашифровать";
+            this.button_Shifr.UseVisualStyleBackColor = true;
+            this.button_Shifr.Click += new System.EventHandler(this.button_Shifr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShifrText);
+            this.Controls.Add(this.Text);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.button_unShifr);
+            this.Controls.Add(this.button_Shifr);
+            this.Name = "Form1";
+     //       this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ShifrText;
+        private System.Windows.Forms.TextBox Text;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Button button_unShifr;
+        private System.Windows.Forms.Button button_Shifr;
+    }
+}
+
---
+++ KRIP_SHIFR_VERNAMA/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace KRIP_SHIFR_VERNAMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Shifr_Click(object sender, EventArgs e)
+        {
+            char[] text = Text.Text.ToCharArray();
+          
+
+         
+           // byte[] result;
+         
+           
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in System.Text.Encoding.Unicode.GetBytes(text))
+                sb.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+        
+            string binaryStr_16 = sb.ToString();
+            int size_binaryStr_8 = binaryStr_16.Length * 1 / 2;
+            char[] binaryStr_8 = new char[size_binaryStr_8];
+            int check_binaryStr_8 = 0;
+            for (int i = 0, k = 0; i < binaryStr_16.Length; i++)
+            {
+                if(check_binaryStr_8 != 8)
+                {
+                    check_binaryStr_8++;
+                    binaryStr_8[k] = binaryStr_16[i];
+                    k++;
+                }
+                else
+                {
+                    i = i + 8 - 1;
+                    check_binaryStr_8 = 0;
+                }
+            }
+
+            //StringBuilder bit = new StringBuilder();
+            //Random rnd = new Random();
+            //for (int i = 0; i < text.Length; i++)
+            //    bit.Append(rnd.Next(0, 2));
+            Random rnd = new Random();
+           string bit = Convert.ToString(rnd.Next(256, 127), 2);
+            keyBox.Text = bit.ToString();
+            
+            ShifrText.Text = binaryStr_16;
+        }
+
+        private void button_unShifr_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
